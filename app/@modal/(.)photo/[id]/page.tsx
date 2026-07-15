da78dbbd3093679f0ba803ada32f198e1c9007de
cxt590 <Christopher.Taylor1@sherwin.com>
2026-07-15 15:41:59 -0400

    pre render images

--- app/@modal/(.)photo/[id]/page.tsx
+++ app/@modal/(.)photo/[id]/page.tsx
@@ -3,6 +3,10 @@ import PictureModal from "@/app/components/pictureModal/page";
 import Image from "next/image";
 import { use } from "react";
 
+export function generateStaticParams() {
+  return Array.from({ length: 10 }, (_, i) => ({ id: String(i + 1) }));
+}
+
 export default async function PhotoModalPage({ params }: { params: Promise<{ id: string }> }) {
   const { id } = use(params)
   
